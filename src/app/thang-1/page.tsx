6e38b0e2c3cabe67ece772d67881bc3121672c1f
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-12-27 04:43:31 +0700

    chore: moved 1 mont

---
+++ src/app/thang-1/page.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import {Metadata} from "next";
+import styles from '../styles/january.module.css';
+import thang1Image from '../../../public/assets/images/january.jpg';
+
+export const metadata: Metadata = {
+    title: "Thông điệp tháng 1",
+};
+export default function January() {
+  return (
+      <div className={styles.cauChuyen}>
+          <div className={styles.thongDiep}>
+              <span>
+                  HÔM NAY TÔI
+                  <br/>
+                  ĐÓNG CỬA QUÁ KHỨ,
+                  <br/>
+                  MỞ CỬA TƯƠNG LAI,
+                  <br/>
+                  HÍT MỘT HƠI THẬT SÂU,
+                  <br/>
+                  BƯỚC QUA VÀ BẮT ĐẦU
+                  <br/>
+                  MỘT CHƯƠNG MỚI
+                  <br/>
+                  TRONG CUỘC ĐỜI TÔI
+              </span>
+          </div>
+          <Image
+              src={thang1Image}
+              alt="thang1"
+              fill // Sử dụng fill để lấp đầy container
+              style={{objectFit: 'contain'}} // Giữ nguyên tỷ lệ và vừa vặn
+              priority // Tải hình ảnh này trước
+          />
+      </div>
+  )
+      ;
+}
