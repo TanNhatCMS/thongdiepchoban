1963cea7a19ad0104f0ebbf7438ff962fff1376e
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-12-27 04:42:27 +0700

    update: update config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,52 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next",
+    "next/core-web-vitals"
+  ),
+  {
+    ignorePatterns: ["node_modules/"],
+    env: {
+      es6: true,
+      browser: true,
+      node: true,
+    },
+    globals: {
+      io: true,
+      chrome: true,
+    },
+    parserOptions: {
+      ecmaVersion: 10,
+      ecmaFeatures: {
+        jsx: true,
+      },
+      project: ["./tsconfig.json"],
+      tsconfigRootDir: __dirname,
+      createDefaultProgram: true,
+      sourceType: "module",
+      babelOptions: {
+        presets: [require.resolve("next/babel")],
+      },
+    },
+    rules: {},
+    overrides: [
+      {
+        files: ["**/*.ts?(x)", "**/*.js?(x)"],
+        rules: {
+          "react/react-in-jsx-scope": "off",
+          "react/jsx-sort-props": "off",
+          "react/function-component-definition": "off",
+          "no-shadow": "off",
+        },
+      },
+    ],
+  },
 ];
 
 export default eslintConfig;
