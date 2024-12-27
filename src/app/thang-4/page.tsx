4e2ded698b62c2362f88645e4bf1f5cca5b6f3dc
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-12-27 08:15:08 +0700

    fix: fix layout April

--- src/app/thang-4/page.tsx
+++ src/app/thang-4/page.tsx
@@ -1,30 +1,29 @@
-import Image from "next/image";
-import {Metadata} from "next";
+import Image from 'next/image';
+import { Metadata } from 'next';
 import styles from '../styles/april.module.css';
 import aprilImage from '../../../public/assets/images/april.jpg'; // Import hình ảnh
 
 export const metadata: Metadata = {
-    title: "Thông điệp tháng 4",
+  title: 'Thông điệp tháng 4',
 };
 export default function April() {
   return (
     <div className={styles.cauChuyen}>
       <div className={styles.thongDiep}>
-        <span className={styles.text}>
+        <p>
           Cuộc sống là một quyển sách
           <br />
           và những người không học hỏi
           <br />
           chỉ đọc một trang.
-          <br />
-          - Saint Augustine -
-        </span>
+          <br /> - Saint Augustine -
+        </p>
       </div>
       <Image
         src={aprilImage}
         alt="Tháng 4"
-        layout="fill"
-        objectFit="cover"
+        fill
+        style={{ objectFit: 'contain' }}
         priority
       />
     </div>
