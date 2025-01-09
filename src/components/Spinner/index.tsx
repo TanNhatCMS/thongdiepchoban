2c1ecaf3bad95fd2c9e2beeef3d33bca88dbca5b
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2025-01-10 05:46:32 +0700

    chore fix spinner

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import styles from './spinner.module.css'
-
+import logo from '../../assets/images/itc.svg'
 const Spinner: React.FC = () => {
   return (
-    <>
-      <div className={styles.spinner}></div>
-      <div className={styles.spinner_overlay}></div>
-    </>
+      <div className={styles.spinner_overlay}>
+        <div className={styles.spinner}>
+          <img src={logo} alt="Logo" className={styles.spinner_logo} />
+        </div>
+      </div>
   )
 }
 
